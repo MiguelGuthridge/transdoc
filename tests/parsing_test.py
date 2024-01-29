6707dcc178d123618d805703f104d80b3a467275
Miguel Guthridge <hello@miguelguthridge.com>
2024-01-30 00:00:28 +1100

    Tidy up test cases

--- tests/parsing_test.py
+++ tests/parsing_test.py
@@ -7,79 +7,169 @@ from transdoc import transform
 from inspect import getsource
 
 
+###############################################################################
+
+# Some simple rules for testing
+
 def hi():
     """Simple rule"""
     return "hi"
 
 
 def hello(name: str):
-    """Simple rule"""
+    """Simple rule, accepting a parameter"""
     return f"hello, {name}"
 
 
-def no_transformation():
-    """hi"""
+###############################################################################
 
 
-def transformation_hi():
+def test_no_transform():
+    """Does transdoc leave functions that don't have transformations alone?"""
+    # Pylance yells about this but Mypy seems fine
+    assert transform(test_no_transform, [hi]) \
+        == getsource(test_no_transform)
+
+
+###############################################################################
+
+
+def fn_simple():
     """{{hi}}"""
 
 
-transformation_hi_result = \
-    '''def transformation_hi():
-    """hi"""
-'''
-
-
-def transformation_hello():
-    """{{hello[Miguel]}}"""
-
-
-transformation_hello_result = \
-    '''def transformation_hello():
-    """hello, Miguel"""
-'''
-
-
-def transformation_hello_call():
-    """{{hello("Miguel")}}"""
-
-
-transformation_hello_call_result = \
-    '''def transformation_hello_call():
-    """hello, Miguel"""
-'''
-
-
-def transformation_hello_call_kwargs():
-    """{{hello(name="Miguel")}}"""
-
-
-transformation_hello_call_kwargs_result = \
-    '''def transformation_hello_call_kwargs():
-    """hello, Miguel"""
-'''
-
-
-def test_no_transformation():
-    # Pylance yells about this but Mypy seems fine
-    assert transform(no_transformation, [hi]) == getsource(no_transformation)
+fn_simple_result = "\n".join([
+    'def fn_simple():',
+    '    """hi"""',
+    '',
+])
 
 
 def test_simple_transformation():
-    assert transform(transformation_hi, [hi]) == transformation_hi_result
+    """Test a simple transformation"""
+    assert transform(fn_simple, [hi]) == fn_simple_result
 
 
-def test_transformation_square_bracket_syntax():
-    assert transform(transformation_hello, [hello]) \
-        == transformation_hello_result
+###############################################################################
+
+
+def fn_square_bracket():
+    """{{hello[Miguel]}}"""
+
+
+fn_square_bracket_result = \
+    '''def fn_square_bracket():
+    """hello, Miguel"""
+'''
+
+
+def test_square_bracket_syntax():
+    """
+    Test a transformation which provides a text argument using the square
+    bracket syntax
+    """
+    assert transform(fn_square_bracket, [hello])  == fn_square_bracket_result
+
+
+###############################################################################
+
+
+def fn_call_simple():
+    """{{hello("Miguel")}}"""
+
+
+fn_call_simple_result = \
+    '''def fn_call_simple():
+    """hello, Miguel"""
+'''
 
 
 def test_transformation_function_call():
-    assert transform(transformation_hello_call, [hello]) \
-        == transformation_hello_call_result
+    """
+    Test a transformation which provides arguments using the function-call
+    syntax
+    """
+    assert transform(fn_call_simple, [hello])  == fn_call_simple_result
+
+
+###############################################################################
+
+
+def fn_call_kwargs():
+    """{{hello(name="Miguel")}}"""
+
+
+fn_call_kwargs_result = \
+    '''def fn_call_kwargs():
+    """hello, Miguel"""
+'''
 
 
 def test_transformation_function_call_kwargs():
-    assert transform(transformation_hello_call_kwargs, [hello]) \
-        == transformation_hello_call_kwargs_result
+    """
+    Test a transformation which provides arguments using the function-call
+    syntax, passing the argument as a kwarg.
+    """
+    assert transform(fn_call_kwargs, [hello]) \
+        == fn_call_kwargs_result
+
+
+###############################################################################
+
+
+class Greeter:
+    """
+    This class is useful for saying hi to people and stuff
+
+    {{hi}}
+    """
+
+    def __init__(self, my_name: str) -> None:
+        """
+        Create a Greeter, which introduces itself to people
+
+        {{hi}}
+        """
+        self.__my_name = my_name
+
+    def say_hi(self, name: str) -> str:
+        """
+        Introduce this greeter to someone.
+
+        {{hi}}
+        """
+        return f"Hi, {name}, I'm {self.__my_name}"
+
+
+class_result = '\n'.join([
+    'class Greeter:',
+    '    """',
+    '    This class is useful for saying hi to people and stuff',
+    '',
+    '    hi',
+    '    """',
+    '',
+    '    def __init__(self, my_name: str) -> None:',
+    '        """',
+    '        Create a Greeter, which introduces itself to people',
+    '',
+    '        hi',
+    '        """',
+    '        self.__my_name = my_name',
+    '',
+    '    def say_hi(self, name: str) -> str:',
+    '        """',
+    '        Introduce this greeter to someone.',
+    '',
+    '        hi',
+    '        """',
+    '        return f"Hi, {name}, I\'m {self.__my_name}"',
+    '',
+])
+
+
+def test_transform_class():
+    """
+    Does Transdoc transform docstrings for classes and their methods correctly?
+    """
+    assert transform(Greeter, [hi]) == class_result
